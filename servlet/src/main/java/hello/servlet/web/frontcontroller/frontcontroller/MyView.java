2ae71a9512c79a657dda865f00e103465d0fb194
유승우 <66248758+Seung-Woo-kmu@users.noreply.github.com>
2022-09-25 17:28:43 +0900

    Add handler
    Add handler

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/frontcontroller/MyView.java
@@ -0,0 +1,36 @@
+package hello.servlet.web.frontcontroller;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+    public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        modelToRequestAttribute(model, req);
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) {
+        model.forEach((key, value) -> req.setAttribute(key, value));
+        model.forEach((key, value) -> System.out.println(value));
+    }
+}
